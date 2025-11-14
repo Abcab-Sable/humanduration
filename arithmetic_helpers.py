bc7eed162e81acf616bbedf8447ac59b8d93badc
Gabriel <272199@student.calderdale.ac.uk>
2025-11-14 20:59:49 +0000

    made branching simpler for checking value is in range.
    Implemented value checks and type checks, alongside error handling

--- arithmetic_helpers.py
+++ arithmetic_helpers.py
@@ -1,9 +1,13 @@
 def clamp(value: float, minimum: float, maximum: float) -> float:
-    if value >= minimum and value <= maximum:
-        return value
-    elif value < minimum:
+    if minimum > maximum:
+        raise ValueError("Minimum parameter at clamp must be lower than the Maximum parameter at clamp")
+    
+    if not isinstance(value, float) or not isinstance(minimum, float) or not isinstance(maximum, float):
+        raise TypeError("All parameters at clamp must be of a float type")
+
+    if value < minimum:
         return minimum
     elif value > maximum:
         return maximum
     else:
-        print("Something went wrong at clamp")
+        return value
