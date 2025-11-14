8d55731433963d56a33818340cc646432ea1a254
Gabriel <272199@student.calderdale.ac.uk>
2025-11-14 21:07:03 +0000

    changed type checking to use duck typing conventions instead of strictly enforcing types.

--- arithmetic_helpers.py
+++ arithmetic_helpers.py
@@ -1,13 +1,16 @@
 def clamp(value: float, minimum: float, maximum: float) -> float:
-    if minimum > maximum:
-        raise ValueError("Minimum parameter at clamp must be lower than the Maximum parameter at clamp")
-    
-    if not isinstance(value, float) or not isinstance(minimum, float) or not isinstance(maximum, float):
-        raise TypeError("All parameters at clamp must be of a float type")
+    try:
+        if minimum > maximum:
+            raise ValueError("Minimum parameter at clamp must be lower than the Maximum parameter at clamp")
 
-    if value < minimum:
-        return minimum
-    elif value > maximum:
-        return maximum
-    else:
-        return value
+        if value < minimum:
+            return minimum
+        elif value > maximum:
+            return maximum
+        else:
+            return value
+    
+    except TypeError:
+        raise TypeError("All parameters at clamp must be of a float type")
+    except:
+        raise Exception("Something weird went wrong at clamp")
