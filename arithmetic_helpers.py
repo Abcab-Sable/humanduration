1f61f3421e8112adfb2a578e1b4209fbd4f8688e
Gabriel <272199@student.calderdale.ac.uk>
2025-11-14 21:23:00 +0000

    documented edge cases for numbers
    implemented duck-typing-convention friendly type checking

--- arithmetic_helpers.py
+++ arithmetic_helpers.py
@@ -1,14 +1,20 @@
+# Edge cases for numbers:
+#   For NaN, the function should raise an error.
+#   For inf, the function should raise an error if it is the minimum.
+#   For -inf, the function should raise an error if it is the maximum.
+
 def clamp(value: float, minimum: float, maximum: float) -> float:
     try:
         if minimum > maximum:
             raise ValueError("Minimum parameter at clamp must be lower than the Maximum parameter at clamp")
+    except TypeError:
+        raise TypeError("minimum and maximum parameters at clamp must be of a float-like type, such that they are numerically comparable")
 
+    try:
         if value < minimum:
             return minimum
         elif value > maximum:
             return maximum
-        else:
-            return value
-    
+        return value
     except TypeError:
-        raise TypeError("All parameters at clamp must be of a float type")
+        raise TypeError("value parameter at clamp must be of a float-like type, such that it is numerically comparable to minimum and maximum")
