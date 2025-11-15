b61a16f1a3f1065b4d86a70a6fe31da77a8129be
Gabriel <272199@student.calderdale.ac.uk>
2025-11-15 09:30:40 +0000

    removed duck-type-checking for now

--- arithmetic_helpers.py
+++ arithmetic_helpers.py
@@ -6,39 +6,37 @@ import math
 #       if maximum is inf, that just means the upper bound is unbounded.
 #   For -inf, the function should raise an error if it is the maximum.
 #       if minimum is -inf, that just means the lower bound is unbounded.
+#   If value is inf, maximum should be returned.
+#   If value is -inf, minimum should be returned.
+
 
 def clamp(value: float, minimum: float, maximum: float) -> float:
-    try:
-        if math.isnan(minimum) or math.isnan(maximum):
-            raise ValueError("minimum and maximum parameters at clamp cannot be a NaN")
 
-        if math.isinf(minimum):
-            raise ValueError("minimum parameter at clamp cannot be positive-infinity")
+    if math.isnan(minimum) or math.isnan(maximum):
+        raise ValueError("minimum and maximum parameters at clamp cannot be a NaN")
 
-        if math.isinf(-maximum):
-            raise ValueError("maximum parameter at clamp cannot be negative-infinity")
+    if math.isinf(minimum):
+        raise ValueError("minimum parameter at clamp cannot be positive-infinity")
 
-        if minimum > maximum:
-            raise ValueError("Minimum parameter at clamp must be lower than the Maximum parameter at clamp")
+    if math.isinf(-maximum):
+        raise ValueError("maximum parameter at clamp cannot be negative-infinity")
 
-    except TypeError:
-        raise TypeError("minimum and maximum parameters at clamp must be of a float-like type, such that they are numerically comparable")
+    if minimum > maximum:
+        raise ValueError(
+            "Minimum parameter at clamp must be lower than the Maximum parameter at clamp"
+        )
 
-    try:
-        if math.isnan(value):
-            raise ValueError("value parameter at clamp cannot be a NaN")
+    if math.isnan(value):
+        raise ValueError("value parameter at clamp cannot be a NaN")
 
-        if math.isinf(value):
-            return maximum
-        
-        if math.isinf(-value):
-            return minimum
+    if math.isinf(value):
+        return maximum
 
-        if value < minimum:
-            return minimum
-        elif value > maximum:
-            return maximum
-        return value
+    if math.isinf(-value):
+        return minimum
 
-    except TypeError:
-        raise TypeError("value parameter at clamp must be of a float-like type, such that it is numerically comparable to minimum and maximum")
+    if value < minimum:
+        return minimum
+    elif value > maximum:
+        return maximum
+    return value
