7473862bf0eab92ccd2a5f139d55c905a485c3cc
Gabriel <272199@student.calderdale.ac.uk>
2025-11-17 17:54:59 +0000

    create basic scan function

--- tokens.py
+++ tokens.py
@@ -1,3 +1,39 @@
 from .units import UNITS as u
+from .exceptions import DurationParseError
 
-print(u)
+test = "hr30min15secs"
+
+def scan(dur_str):
+
+    duration_tokens = []
+    duration_token = []
+    digit_token = []
+    unit_token = []
+
+    length = len(dur_str)
+    for i, character in enumerate(dur_str):
+        last_char = i == (length - 1)
+        
+        if character.isdigit():
+            if unit_token:
+                duration_token.append(unit_token)
+                duration_tokens.append(duration_token)
+                unit_token = []
+                duration_token = []
+            digit_token.append(character)
+            counter += 1
+            continue
+        if not character.isdigit():
+            if digit_token:
+                duration_token.append(digit_token)
+                digit_token = []
+            if character != " ":
+                unit_token.append(character)
+
+            if last_char:
+                duration_token.append(unit_token)
+                duration_tokens.append(duration_token)
+
+    return duration_tokens
+
+print(scan(test))
