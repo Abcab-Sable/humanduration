4530666551257ea2226e165e40cd3828211ca1bc
Gabriel <272199@student.calderdale.ac.uk>
2025-11-20 20:00:57 +0000

    Add flush_unit_token function to adhere to DRY principle

--- tokens.py
+++ tokens.py
@@ -1,7 +1,16 @@
 from .units import UNITS as u
 from .exceptions import DurationParseError
 
-test = "1hr2m3s"
+test = "-1hr-2.1m-3s"
+
+def flush_unit_token(unit_token, valid_units, duration_token, duration_tokens):
+    joined_unit = "".join(unit_token)
+    if joined_unit in valid_units:
+        duration_token.append(joined_unit)
+        duration_tokens.append(duration_token)
+    else:
+        raise DurationParseError(f"Invalid unit: {joined_unit}")
+    return [], [], duration_tokens
 
 def scan(dur_str):
 
@@ -23,54 +32,45 @@ def scan(dur_str):
 
         if character.isdigit():
             if unit_token:
-                joined_unit = "".join(unit_token)
-                if joined_unit in valid_units:
-                    duration_token.append(joined_unit)
-                    duration_tokens.append(duration_token)
-                    unit_token = []
-                    duration_token = []
-                else:
-                    raise DurationParseError(f"Invalid unit: {joined_unit}")
+                unit_token, duration_token, duration_tokens = flush_unit_token(unit_token, valid_units, duration_token, duration_tokens)
             digit_token.append(character)
             continue
+
+
         if character in ["-", "+", "."]:
+            if last_char:
+                raise DurationParseError(f'Invalid unit: {"".join(unit_token) + character}')
             if unit_token:
                 if not dur_str[i + 1].isdigit():
                     
                     unit_token.append(character)
                     continue
                 else:
-                    joined_unit = "".join(unit_token)
-                    if joined_unit in valid_units:
-                        duration_token.append(joined_unit)
-                        duration_tokens.append(duration_token)
-                        unit_token = []
-                        duration_token = []
-                    else:
-                        raise DurationParseError(f"Invalid unit: {joined_unit}")
+                    unit_token, duration_token, duration_tokens = flush_unit_token(unit_token, valid_units, duration_token, duration_tokens)
                 digit_token.append(character)
             else:
                 digit_token.append(character)
             continue
+
+
         if not character.isdigit():
             if digit_token:
+                
                 float_token = "".join(digit_token)
+
                 try:
                     float_token = float(float_token)
                 except:
                     raise DurationParseError(f"Invalid number: {float_token}")
+
                 duration_token.append(float_token)
                 digit_token = []
+
             if character != " ":
                 unit_token.append(character)
 
             if last_char:
-                joined_unit = "".join(unit_token)
-                if joined_unit in valid_units:
-                    duration_token.append(joined_unit)
-                    duration_tokens.append(duration_token)
-                else:
-                    raise DurationParseError(f"Invalid unit: {joined_unit}")
+                unit_token, duration_token, duration_tokens = flush_unit_token(unit_token, valid_units, duration_token, duration_tokens)
 
     return duration_tokens
 
